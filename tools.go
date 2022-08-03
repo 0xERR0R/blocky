3c5b7a20dcc118be6510fea5e036a33879a0ff66
Dimitri Herzog <dimitri.herzog@gmail.com>
2022-08-03 22:10:07 +0200

    chore: Managing of external tools with go modules (#616) (#617)

---
+++ tools.go
@@ -0,0 +1,13 @@
+//go:build tools
+// +build tools
+
+// see https://play-with-go.dev/tools-as-dependencies_go115_en/
+// and https://www.jvt.me/posts/2022/06/15/go-tools-dependency-management/
+package tools
+
+import (
+	_ "github.com/abice/go-enum"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/onsi/ginkgo/v2/ginkgo"
+	_ "github.com/swaggo/swag/cmd/swag"
+)
