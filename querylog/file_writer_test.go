c0fb4703111adb473ddd0f7f45caa14559d54307
Kwitsch <kwitsch@gmail.com>
2022-08-06 01:46:26 +0200

    remove comment for DeferCleanup

--- querylog/file_writer_test.go
+++ querylog/file_writer_test.go
@@ -29,7 +29,7 @@ var _ = Describe("FileWriter", func() {
 	JustBeforeEach(func() {
 		tmpDir = helpertest.NewTmpFolder("fileWriter")
 		Expect(tmpDir.Error).Should(Succeed())
-		// DeferCleanup(tmpDir.Clean)
+		DeferCleanup(tmpDir.Clean)
 	})
 
 	Describe("CSV writer", func() {
