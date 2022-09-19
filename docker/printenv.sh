7f96b06981d1f2949ccc0b274d1b57a4e8c6c978
Kwitsch <kwitsch@gmail.com>
2022-09-19 19:28:45 +0200

    next

--- docker/printenv.sh
+++ docker/printenv.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/ash
 
 echo "::group::Environment - ${TARGETARCH}${TARGETVARIANT}"
-echo "CC: ${go env CC}"
-echo "CXX: ${go env CXX}"
-echo "CGO_ENABLED: ${go env CGO_ENABLED}"
-echo "GOOS: ${go env GOOS}"
-echo "GOARCH: ${go env GOARCH}"
-echo "GOARM: ${go env GOARM}"
+echo "CC: $(go env CC)"
+echo "CXX: $(go env CXX)"
+echo "CGO_ENABLED: $(go env CGO_ENABLED)"
+echo "GOOS: $(go env GOOS)"
+echo "GOARCH: $(go env GOARCH)"
+echo "GOARM: $(go env GOARM)"
 echo "::endgroup::"
