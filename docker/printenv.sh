88af824a10e005b38c7cc154a6e93f4127f05db6
Kwitsch <kwitsch@gmail.com>
2022-09-23 19:52:19 +0200

    cleanup

--- docker/printenv.sh
+++
@@ -1,8 +0,0 @@
-echo "### Environment - ${TARGETARCH}${TARGETVARIANT} ###"
-echo "# CC: $(go env CC)"
-echo "# CXX: $(go env CXX)"
-echo "# CGO_ENABLED: $(go env CGO_ENABLED)"
-echo "# GOOS: $(go env GOOS)"
-echo "# GOARCH: $(go env GOARCH)"
-echo "# GOARM: $(go env GOARM)"
-echo "####################"
