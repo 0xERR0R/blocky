99eff1f243265b18314cc4bc1ea2d2ce57b0387e
Kwitsch <kwitsch@gmail.com>
2022-09-30 20:45:50 +0200

    tag change

--- config/config.go
+++ config/config.go
@@ -449,68 +449,68 @@ func extractNet(upstream string) (NetProtocol, string) {
 // Config main configuration
 // nolint:maligned
 type Config struct {
-	Upstream            UpstreamConfig            `yaml:"upstream"`
-	UpstreamTimeout     Duration                  `yaml:"upstreamTimeout" default:"2s"`
-	ConnectIPVersion    IPVersion                 `yaml:"connectIPVersion"`
-	CustomDNS           CustomDNSConfig           `yaml:"customDNS"`
-	Conditional         ConditionalUpstreamConfig `yaml:"conditional"`
-	Blocking            BlockingConfig            `yaml:"blocking"`
-	ClientLookup        ClientLookupConfig        `yaml:"clientLookup"`
-	Caching             CachingConfig             `yaml:"caching"`
-	QueryLog            QueryLogConfig            `yaml:"queryLog"`
-	Prometheus          PrometheusConfig          `yaml:"prometheus"`
-	Redis               RedisConfig               `yaml:"redis"`
-	LogLevel            log.Level                 `yaml:"logLevel" default:"info"`
-	LogFormat           log.FormatType            `yaml:"logFormat" default:"text"`
-	LogPrivacy          bool                      `yaml:"logPrivacy" default:"false"`
-	LogTimestamp        bool                      `yaml:"logTimestamp" default:"true"`
-	DNSPorts            ListenConfig              `yaml:"port" koanf:"port" default:"[\"53\"]"`
-	HTTPPorts           ListenConfig              `yaml:"httpPort"`
-	HTTPSPorts          ListenConfig              `yaml:"httpsPort"`
-	TLSPorts            ListenConfig              `yaml:"tlsPort"`
-	DoHUserAgent        string                    `yaml:"dohUserAgent"`
-	MinTLSServeVer      string                    `yaml:"minTlsServeVersion" koanf:"minTlsServeVersion" default:"1.2"`
-	StartVerifyUpstream bool                      `yaml:"startVerifyUpstream" default:"false"`
+	Upstream            UpstreamConfig            `koanf:"upstream"`
+	UpstreamTimeout     Duration                  `koanf:"upstreamTimeout" default:"2s"`
+	ConnectIPVersion    IPVersion                 `koanf:"connectIPVersion"`
+	CustomDNS           CustomDNSConfig           `koanf:"customDNS"`
+	Conditional         ConditionalUpstreamConfig `koanf:"conditional"`
+	Blocking            BlockingConfig            `koanf:"blocking"`
+	ClientLookup        ClientLookupConfig        `koanf:"clientLookup"`
+	Caching             CachingConfig             `koanf:"caching"`
+	QueryLog            QueryLogConfig            `koanf:"queryLog"`
+	Prometheus          PrometheusConfig          `koanf:"prometheus"`
+	Redis               RedisConfig               `koanf:"redis"`
+	LogLevel            log.Level                 `koanf:"logLevel" default:"info"`
+	LogFormat           log.FormatType            `koanf:"logFormat" default:"text"`
+	LogPrivacy          bool                      `koanf:"logPrivacy" default:"false"`
+	LogTimestamp        bool                      `koanf:"logTimestamp" default:"true"`
+	DNSPorts            ListenConfig              `koanf:"port" default:"[\"53\"]"`
+	HTTPPorts           ListenConfig              `koanf:"httpPort"`
+	HTTPSPorts          ListenConfig              `koanf:"httpsPort"`
+	TLSPorts            ListenConfig              `koanf:"tlsPort"`
+	DoHUserAgent        string                    `koanf:"dohUserAgent"`
+	MinTLSServeVer      string                    `koanf:"minTlsServeVersion" default:"1.2"`
+	StartVerifyUpstream bool                      `koanf:"startVerifyUpstream" default:"false"`
 	// Deprecated
-	DisableIPv6  bool            `yaml:"disableIPv6" default:"false"`
-	CertFile     string          `yaml:"certFile"`
-	KeyFile      string          `yaml:"keyFile"`
-	BootstrapDNS BootstrapConfig `yaml:"bootstrapDns"`
-	HostsFile    HostsFileConfig `yaml:"hostsFile"`
-	FqdnOnly     bool            `yaml:"fqdnOnly" default:"false"`
-	Filtering    FilteringConfig `yaml:"filtering"`
-	Ede          EdeConfig       `yaml:"ede"`
+	DisableIPv6  bool            `koanf:"disableIPv6" default:"false"`
+	CertFile     string          `koanf:"certFile"`
+	KeyFile      string          `koanf:"keyFile"`
+	BootstrapDNS BootstrapConfig `koanf:"bootstrapDns"`
+	HostsFile    HostsFileConfig `koanf:"hostsFile"`
+	FqdnOnly     bool            `koanf:"fqdnOnly" default:"false"`
+	Filtering    FilteringConfig `koanf:"filtering"`
+	Ede          EdeConfig       `koanf:"ede"`
 }
 
 type BootstrapConfig bootstrapConfig // to avoid infinite recursion. See BootstrapConfig.UnmarshalYAML.
 type bootstrapConfig struct {
-	Upstream Upstream `yaml:"upstream"`
-	IPs      []net.IP `yaml:"ips"`
+	Upstream Upstream `koanf:"upstream"`
+	IPs      []net.IP `koanf:"ips"`
 }
 
 // PrometheusConfig contains the config values for prometheus
 type PrometheusConfig struct {
-	Enable bool   `yaml:"enable" default:"false"`
-	Path   string `yaml:"path" default:"/metrics"`
+	Enable bool   `koanf:"enable" default:"false"`
+	Path   string `koanf:"path" default:"/metrics"`
 }
 
 // UpstreamConfig upstream server configuration
 type UpstreamConfig struct {
-	ExternalResolvers map[string][]Upstream `yaml:",inline" koanf:",remain"`
+	ExternalResolvers map[string][]Upstream `koanf:",remain"`
 }
 
 // RewriteConfig custom DNS configuration
 type RewriteConfig struct {
-	Rewrite          map[string]string `yaml:"rewrite"`
-	FallbackUpstream bool              `yaml:"fallbackUpstream" default:"false"`
+	Rewrite          map[string]string `koanf:"rewrite"`
+	FallbackUpstream bool              `koanf:"fallbackUpstream" default:"false"`
 }
 
 // CustomDNSConfig custom DNS configuration
 type CustomDNSConfig struct {
-	RewriteConfig       `yaml:",inline" koanf:",squash"`
-	CustomTTL           Duration         `yaml:"customTTL" default:"1h"`
-	Mapping             CustomDNSMapping `yaml:"mapping"`
-	FilterUnmappedTypes bool             `yaml:"filterUnmappedTypes" default:"true"`
+	RewriteConfig       `koanf:",squash"`
+	CustomTTL           Duration         `koanf:"customTTL" default:"1h"`
+	Mapping             CustomDNSMapping `koanf:"mapping"`
+	FilterUnmappedTypes bool             `koanf:"filterUnmappedTypes" default:"true"`
 }
 
 // CustomDNSMapping mapping for the custom DNS configuration
@@ -520,8 +520,8 @@ type CustomDNSMapping struct {
 
 // ConditionalUpstreamConfig conditional upstream configuration
 type ConditionalUpstreamConfig struct {
-	RewriteConfig `yaml:",inline" koanf:",squash"`
-	Mapping       ConditionalUpstreamMapping `yaml:"mapping"`
+	RewriteConfig `koanf:",squash"`
+	Mapping       ConditionalUpstreamMapping `koanf:"mapping"`
 }
 
 // ConditionalUpstreamMapping mapping for conditional configuration
@@ -531,72 +531,72 @@ type ConditionalUpstreamMapping struct {
 
 // BlockingConfig configuration for query blocking
 type BlockingConfig struct {
-	BlackLists        map[string][]string `yaml:"blackLists"`
-	WhiteLists        map[string][]string `yaml:"whiteLists"`
-	ClientGroupsBlock map[string][]string `yaml:"clientGroupsBlock"`
-	BlockType         string              `yaml:"blockType" default:"ZEROIP"`
-	BlockTTL          Duration            `yaml:"blockTTL" default:"6h"`
-	DownloadTimeout   Duration            `yaml:"downloadTimeout" default:"60s"`
-	DownloadAttempts  uint                `yaml:"downloadAttempts" default:"3"`
-	DownloadCooldown  Duration            `yaml:"downloadCooldown" default:"1s"`
-	RefreshPeriod     Duration            `yaml:"refreshPeriod" default:"4h"`
+	BlackLists        map[string][]string `koanf:"blackLists"`
+	WhiteLists        map[string][]string `koanf:"whiteLists"`
+	ClientGroupsBlock map[string][]string `koanf:"clientGroupsBlock"`
+	BlockType         string              `koanf:"blockType" default:"ZEROIP"`
+	BlockTTL          Duration            `koanf:"blockTTL" default:"6h"`
+	DownloadTimeout   Duration            `koanf:"downloadTimeout" default:"60s"`
+	DownloadAttempts  uint                `koanf:"downloadAttempts" default:"3"`
+	DownloadCooldown  Duration            `koanf:"downloadCooldown" default:"1s"`
+	RefreshPeriod     Duration            `koanf:"refreshPeriod" default:"4h"`
 	// Deprecated
-	FailStartOnListError  bool              `yaml:"failStartOnListError" default:"false"`
-	ProcessingConcurrency uint              `yaml:"processingConcurrency" default:"4"`
-	StartStrategy         StartStrategyType `yaml:"startStrategy" default:"blocking"`
+	FailStartOnListError  bool              `koanf:"failStartOnListError" default:"false"`
+	ProcessingConcurrency uint              `koanf:"processingConcurrency" default:"4"`
+	StartStrategy         StartStrategyType `koanf:"startStrategy" default:"blocking"`
 }
 
 // ClientLookupConfig configuration for the client lookup
 type ClientLookupConfig struct {
-	ClientnameIPMapping map[string][]net.IP `yaml:"clients"`
-	Upstream            Upstream            `yaml:"upstream"`
-	SingleNameOrder     []uint              `yaml:"singleNameOrder"`
+	ClientnameIPMapping map[string][]net.IP `koanf:"clients"`
+	Upstream            Upstream            `koanf:"upstream"`
+	SingleNameOrder     []uint              `koanf:"singleNameOrder"`
 }
 
 // CachingConfig configuration for domain caching
 type CachingConfig struct {
-	MinCachingTime        Duration `yaml:"minTime"`
-	MaxCachingTime        Duration `yaml:"maxTime"`
-	CacheTimeNegative     Duration `yaml:"cacheTimeNegative" default:"30m"`
-	MaxItemsCount         int      `yaml:"maxItemsCount"`
-	Prefetching           bool     `yaml:"prefetching"`
-	PrefetchExpires       Duration `yaml:"prefetchExpires" default:"2h"`
-	PrefetchThreshold     int      `yaml:"prefetchThreshold" default:"5"`
-	PrefetchMaxItemsCount int      `yaml:"prefetchMaxItemsCount"`
+	MinCachingTime        Duration `koanf:"minTime"`
+	MaxCachingTime        Duration `koanf:"maxTime"`
+	CacheTimeNegative     Duration `koanf:"cacheTimeNegative" default:"30m"`
+	MaxItemsCount         int      `koanf:"maxItemsCount"`
+	Prefetching           bool     `koanf:"prefetching"`
+	PrefetchExpires       Duration `koanf:"prefetchExpires" default:"2h"`
+	PrefetchThreshold     int      `koanf:"prefetchThreshold" default:"5"`
+	PrefetchMaxItemsCount int      `koanf:"prefetchMaxItemsCount"`
 }
 
 // QueryLogConfig configuration for the query logging
 type QueryLogConfig struct {
-	Target           string       `yaml:"target"`
-	Type             QueryLogType `yaml:"type"`
-	LogRetentionDays uint64       `yaml:"logRetentionDays"`
-	CreationAttempts int          `yaml:"creationAttempts" default:"3"`
-	CreationCooldown Duration     `yaml:"creationCooldown" default:"2s"`
+	Target           string       `koanf:"target"`
+	Type             QueryLogType `koanf:"type"`
+	LogRetentionDays uint64       `koanf:"logRetentionDays"`
+	CreationAttempts int          `koanf:"creationAttempts" default:"3"`
+	CreationCooldown Duration     `koanf:"creationCooldown" default:"2s"`
 }
 
 // RedisConfig configuration for the redis connection
 type RedisConfig struct {
-	Address            string   `yaml:"address"`
-	Password           string   `yaml:"password" default:""`
-	Database           int      `yaml:"database" default:"0"`
-	Required           bool     `yaml:"required" default:"false"`
-	ConnectionAttempts int      `yaml:"connectionAttempts" default:"3"`
-	ConnectionCooldown Duration `yaml:"connectionCooldown" default:"1s"`
+	Address            string   `koanf:"address"`
+	Password           string   `koanf:"password" default:""`
+	Database           int      `koanf:"database" default:"0"`
+	Required           bool     `koanf:"required" default:"false"`
+	ConnectionAttempts int      `koanf:"connectionAttempts" default:"3"`
+	ConnectionCooldown Duration `koanf:"connectionCooldown" default:"1s"`
 }
 
 type HostsFileConfig struct {
-	Filepath       string   `yaml:"filePath"`
-	HostsTTL       Duration `yaml:"hostsTTL" default:"1h"`
-	RefreshPeriod  Duration `yaml:"refreshPeriod" default:"1h"`
-	FilterLoopback bool     `yaml:"filterLoopback"`
+	Filepath       string   `koanf:"filePath"`
+	HostsTTL       Duration `koanf:"hostsTTL" default:"1h"`
+	RefreshPeriod  Duration `koanf:"refreshPeriod" default:"1h"`
+	FilterLoopback bool     `koanf:"filterLoopback"`
 }
 
 type FilteringConfig struct {
-	QueryTypes QTypeSet `yaml:"queryTypes"`
+	QueryTypes QTypeSet `koanf:"queryTypes"`
 }
 
 type EdeConfig struct {
-	Enable bool `yaml:"enable" default:"false"`
+	Enable bool `koanf:"enable" default:"false"`
 }
 
 // nolint:gochecknoglobals
