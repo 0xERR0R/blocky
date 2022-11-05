8d2065792f628b565b1ed9cf21c7b6ffa0aeaed7
Kwitsch <kwitsch@gmail.com>
2022-11-05 02:27:18 +0100

    fix unit test

--- cmd/version_test.go
+++ cmd/version_test.go
@@ -9,7 +9,6 @@ var _ = Describe("Version command", func() {
 	When("Version command is called", func() {
 		It("should execute without error", func() {
 			c := NewVersionCommand()
-			c.SetArgs(make([]string, 0))
 			err := c.Execute()
 			Expect(err).Should(Succeed())
 		})
