9eb92e67efa0818e868818683e2840630bd51ad5
Dimitri Herzog <dimitri.herzog@gmail.com>
2022-11-10 10:08:34 +0000

    fix: add timezone data to docker container

--- main_static.go
+++ main_static.go
@@ -4,6 +4,9 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"time"
 	_ "time/tzdata"
 
 	reaper "github.com/ramr/go-reaper"
@@ -12,4 +15,13 @@ import (
 //nolint:gochecknoinits
 func init() {
 	go reaper.Reap()
+
+	if tz := os.Getenv("TZ"); tz != "" {
+		var err error
+		time.Local, err = time.LoadLocation(tz)
+
+		if err != nil {
+			fmt.Printf("error loading location '%s': %v\n", tz, err)
+		}
+	}
 }
