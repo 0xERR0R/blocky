cf4e894b313d52fa3d9917cf584a7c2673ad5315
Kwitsch <kwitsch@gmail.com>
2022-11-11 22:23:11 +0100

    added setLocaltime

--- main_static.go
+++ main_static.go
@@ -4,7 +4,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"time"
 	_ "time/tzdata"
@@ -16,12 +15,27 @@ import (
 func init() {
 	go reaper.Reap()
 
-	if tz := os.Getenv("TZ"); tz != "" {
-		var err error
-		time.Local, err = time.LoadLocation(tz)
+	setLocaltime()
+}
 
-		if err != nil {
-			fmt.Printf("error loading location '%s': %v\n", tz, err)
+// set localtime to /etc/localtime if available
+// or modify the system time with the TZ environment variable if it is provided
+func setLocaltime() {
+	// load /etc/localtime without modifying it
+	if lt, err := os.ReadFile("/etc/localtime"); err == nil {
+		if t, err := time.LoadLocationFromTZData("", lt); err == nil {
+			time.Local = t
+
+			return
+		}
+	}
+
+	// use zoneinfo from time/tzdata and set location with the TZ environment variable
+	if tz := os.Getenv("TZ"); tz != "" {
+		if t, err := time.LoadLocation(tz); err == nil {
+			time.Local = t
+
+			return
 		}
 	}
 }
