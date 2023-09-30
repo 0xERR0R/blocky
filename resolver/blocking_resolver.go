e86586e5b194067ffa2bb708b311371951087991
Dimitri Herzog <dimitri.herzog@gmail.com>
2023-09-30 19:16:01 +0200

    rebase on master

--- resolver/blocking_resolver.go
+++ resolver/blocking_resolver.go
@@ -143,13 +143,13 @@ func NewBlockingResolver(
 		},
 		clientGroupsBlock: clientGroupsBlock(cfg),
 		redisClient:       redis,
-		fqdnIPCache:  expirationcache.NewCacheWithOnExpired[[]net.IP](expirationcache.Options{
-			CleanupInterval: defaultBlockingCleanUpInterval,
-		}, func(key string) (val *[]net.IP, ttl time.Duration) {
-			return r.queryForFQIdentifierIPs(key)
-		})
 	}
 
+	res.fqdnIPCache = expirationcache.NewCacheWithOnExpired[[]net.IP](expirationcache.Options{
+		CleanupInterval: defaultBlockingCleanUpInterval,
+	}, func(key string) (val *[]net.IP, ttl time.Duration) {
+		return res.queryForFQIdentifierIPs(key)
+	})
 
 	if res.redisClient != nil {
 		setupRedisEnabledSubscriber(res)
