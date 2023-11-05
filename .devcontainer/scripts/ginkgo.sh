2fe71c9b51847e3c4157c1017e111cf08e5026db
Kwitsch <kwitsch@gmail.com>
2023-11-05 15:47:01 +0000

    restructured settings location

--- .devcontainer/scripts/ginkgo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-
-cd "${WORKSPACE_FOLDER}"
-go run github.com/onsi/ginkgo/v2/ginkgo "$@"
--- .devcontainer/scripts/postStart.sh
+++ .devcontainer/scripts/postStart.sh
@@ -5,5 +5,10 @@ sudo ln -sf /usr/share/host/localtime /etc/localtime
 sudo ln -sf /usr/share/host/timezone /etc/timezone
 
 echo "Downloading Go modules..."
-sudo chown -R vscode:golang /go/pkg/mod
-go mod download
+sudo chown -R vscode:golang /go/pkg
+go mod download -x
+
+echo "Installing Go tools..."
+go install github.com/onsi/ginkgo/v2/ginkgo@latest
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+go install mvdan.cc/gofumpt@latest
