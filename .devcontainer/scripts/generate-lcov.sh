e8e9633e6a2e9654a52b759751f34e11becf23d8
Kwitsch <kwitsch@gmail.com>
2023-11-12 18:27:00 +0000

    bugfix

--- .devcontainer/scripts/generate-lcov.sh
+++ .devcontainer/scripts/generate-lcov.sh
@@ -2,4 +2,4 @@
 
 cd "${WORKSPACE_FOLDER}"
 
-nohup bash -c 'ginkgo --label-filter="!e2e" --no-color --keep-going --timeout=5m --coverprofile=lcov.work --covermode=set --cover -r -p && gcov2lcov -infile=coverage.txt -outfile=lcov.info' > lcov.log 2>&1
+nohup bash -c '(ginkgo --label-filter="!e2e" --no-color --keep-going --timeout=5m --coverprofile=lcov.work --covermode=set --cover -r -p || true) && gcov2lcov -infile=lcov.work -outfile=lcov.info' > lcov.log 2>&1
