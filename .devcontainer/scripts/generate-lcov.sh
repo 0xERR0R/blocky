b5cbb98fdfdb07aab8ef52c2d9e4d6c346958715
Kwitsch <kwitsch@gmail.com>
2023-11-13 14:51:36 +0000

    generate seperate lcov.info for each directory

--- .devcontainer/scripts/generate-lcov.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-cd "${WORKSPACE_FOLDER}"
-
-nohup bash -c 'echo "-- Start tests --" && (ginkgo --label-filter="!e2e" --no-color --keep-going --timeout=5m --coverprofile=lcov.work --covermode=atomic --cover -r -p || true) && echo "-- Start lcov convert --" && gcov2lcov -infile=lcov.work -outfile=lcov.info && echo "-- Finished --"' > lcov.log 2>&1
