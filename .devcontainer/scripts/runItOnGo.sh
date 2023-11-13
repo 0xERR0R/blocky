dc0f92eefa986ecc8115feef97d96792fb6bd942
Kwitsch <kwitsch@gmail.com>
2023-11-13 14:57:11 +0000

    path fix in lcov generation script

--- .devcontainer/scripts/runItOnGo.sh
+++ .devcontainer/scripts/runItOnGo.sh
@@ -23,7 +23,7 @@ FILE_PATH="/tmp/$FILE_NAME"
 echo "-- Start $FOLDER_NAME ($(date '+%T')) --"
 
 TIMEFORMAT=' - Ginkgo tests finished in: %R seconds'
-time ginkgo --label-filter="!e2e" --keep-going --timeout=5m --output-dir=/tmp --coverprofile="$FILE_NAME" --covermode=atomic --cover -r -p || true
+time ginkgo --label-filter="!e2e" --keep-going --timeout=5m --output-dir=/tmp --coverprofile="$FILE_NAME" --covermode=atomic --cover -r -p "$FOLDER_PATH" || true
 
 TIMEFORMAT=' - lcov convert finished in: %R seconds'
 time gcov2lcov -infile="$FILE_PATH" -outfile="$FOLDER_PATH/lcov.info"
