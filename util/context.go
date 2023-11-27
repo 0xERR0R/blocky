9562c8551bfa205fea0b26338adfff1b1ed3a641
Kwitsch <kwitsch@gmail.com>
2023-11-27 10:25:32 +0000

    added context util

---
+++ util/context.go
@@ -0,0 +1,25 @@
+package util
+
+import "context"
+
+// ctxSend sends a value to a channel or returns false if the context is done or the channel is closed.
+func ctxSend[T any](ctx context.Context, ch chan T, val T) (ok bool) {
+	if ctx == nil || ch == nil {
+		return false
+	}
+
+	defer func() {
+		if err := recover(); err != nil {
+			ok = false
+		}
+	}()
+
+	select {
+	case <-ctx.Done():
+		ok = false
+	case ch <- val:
+		ok = true
+	}
+
+	return
+}
