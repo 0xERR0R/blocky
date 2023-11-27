5e5506575e1ec727b78a3be6bfa65915a6d495cf
Kwitsch <kwitsch@gmail.com>
2023-11-27 17:02:59 +0000

    this doesn't belong there and produces flakyness

--- resolver/parallel_best_resolver_test.go
+++ resolver/parallel_best_resolver_test.go
@@ -196,7 +196,6 @@ var _ = Describe("ParallelBestResolver", Label("parallelBestResolver"), func() {
 						WithDelay(timeout / 2)
 					DeferCleanup(slowTestUpstream.Close)
 					upstreams = []config.Upstream{{Host: "wrong"}, slowTestUpstream.Start()}
-					Expect(err).Should(Succeed())
 				})
 				It("Should use result from successful resolver", func() {
 					request := newRequest("example.com.", A)
