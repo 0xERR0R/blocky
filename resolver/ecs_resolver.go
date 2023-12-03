11543356b6b303ffa18613992cbf48aefc269f8f
Kwitsch <kwitsch@gmail.com>
2023-12-03 20:29:31 +0100

    Bugfix in ECS forward (#1290)
    * fixed override bug in forward
    
    * set prettier as default formatter for yaml
    
    * added ecs to example config

--- resolver/ecs_resolver.go
+++ resolver/ecs_resolver.go
@@ -53,17 +53,19 @@ func (r *ECSResolver) Resolve(ctx context.Context, request *model.Request) (*mod
 		// Set the client IP from the Edns0 subnet option if the option is enabled and the correct subnet mask is set
 		if r.cfg.UseAsClient && so != nil && ((so.Family == ecsFamilyIPv4 && so.SourceNetmask == ecsMaskIPv4) ||
 			(so.Family == ecsFamilyIPv6 && so.SourceNetmask == ecsMaskIPv6)) {
+			request.Log.Debugf("using request's edns0 address as internal client IP: %s", so.Address)
 			request.ClientIP = so.Address
 		}
 
 		// Set the Edns0 subnet option if the client IP is IPv4 or IPv6 and the masks are set in the configuration
 		if r.cfg.IPv4Mask > 0 || r.cfg.IPv6Mask > 0 {
-			r.setSubnet(request)
+			r.setSubnet(so, request)
 		}
 
 		// Remove the Edns0 subnet option if the client IP is IPv4 or IPv6 and the corresponding mask is not set
 		// and the forwardEcs option is not enabled
 		if r.cfg.IPv4Mask == 0 && r.cfg.IPv6Mask == 0 && so != nil && !r.cfg.Forward {
+			request.Log.Debug("remove edns0 subnet option")
 			util.RemoveEdns0Option[*dns.EDNS0_SUBNET](request.Req)
 		}
 	}
@@ -73,27 +75,29 @@ func (r *ECSResolver) Resolve(ctx context.Context, request *model.Request) (*mod
 
 // setSubnet appends the subnet information to the request as EDNS0 option
 // if the client IP is IPv4 or IPv6 and the corresponding mask is set in the configuration
-func (r *ECSResolver) setSubnet(request *model.Request) {
-	e := new(dns.EDNS0_SUBNET)
-	e.Code = dns.EDNS0SUBNET
-	e.SourceScope = ecsSourceScope
+func (r *ECSResolver) setSubnet(so *dns.EDNS0_SUBNET, request *model.Request) {
+	var subIP net.IP
+	if so != nil && r.cfg.Forward && so.Address != nil {
+		subIP = so.Address
+	} else {
+		subIP = request.ClientIP
+	}
 
-	if ip := request.ClientIP.To4(); ip != nil && r.cfg.IPv4Mask > 0 {
-		mip, err := maskIP(ip, r.cfg.IPv4Mask)
-		if err == nil {
-			e.Family = ecsFamilyIPv4
-			e.SourceNetmask = uint8(r.cfg.IPv4Mask)
-			e.Address = mip
-			util.SetEdns0Option(request.Req, e)
+	var edsOption *dns.EDNS0_SUBNET
+
+	if ip := subIP.To4(); ip != nil && r.cfg.IPv4Mask > 0 {
+		if mip, err := maskIP(ip, r.cfg.IPv4Mask); err == nil {
+			edsOption = newEdnsSubnetOption(mip, ecsFamilyIPv4, r.cfg.IPv4Mask)
 		}
-	} else if ip := request.ClientIP.To16(); ip != nil && r.cfg.IPv6Mask > 0 {
-		mip, err := maskIP(ip, r.cfg.IPv6Mask)
-		if err == nil {
-			e.Family = ecsFamilyIPv6
-			e.SourceNetmask = uint8(r.cfg.IPv6Mask)
-			e.Address = mip
-			util.SetEdns0Option(request.Req, e)
+	} else if ip := subIP.To16(); ip != nil && r.cfg.IPv6Mask > 0 {
+		if mip, err := maskIP(ip, r.cfg.IPv6Mask); err == nil {
+			edsOption = newEdnsSubnetOption(mip, ecsFamilyIPv6, r.cfg.IPv6Mask)
 		}
+	}
+
+	if edsOption != nil {
+		request.Log.Debugf("set edns0 subnet option address: %s", edsOption.Address)
+		util.SetEdns0Option(request.Req, edsOption)
 	}
 }
 
@@ -102,4 +106,15 @@ func maskIP[maskType ECSMask](ip net.IP, mask maskType) (net.IP, error) {
 	_, mip, err := net.ParseCIDR(fmt.Sprintf("%s/%d", ip.String(), mask))
 
 	return mip.IP, err
+}
+
+// newEdnsSubnetOption( creates a new EDNS0 subnet option with the given IP, family and mask
+func newEdnsSubnetOption[maskType ECSMask](ip net.IP, family uint16, mask maskType) *dns.EDNS0_SUBNET {
+	return &dns.EDNS0_SUBNET{
+		Code:          dns.EDNS0SUBNET,
+		SourceScope:   ecsSourceScope,
+		Family:        family,
+		SourceNetmask: uint8(mask),
+		Address:       ip,
+	}
 }
