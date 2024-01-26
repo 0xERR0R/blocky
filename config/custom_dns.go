fc1dd6f890b8354bb4e64f982034a6d6ed973dbe
Ben McHone <ben@mchone.dev>
2024-01-25 21:03:48 -0600

    Make custom_dns more easily extensible for other dns types

--- config/custom_dns.go
+++ config/custom_dns.go
@@ -46,33 +46,55 @@ func (c *CustomDNSMapping) UnmarshalYAML(unmarshal func(interface{}) error) erro
 	}
 	result := make(map[string][]dns.RR, len(input))
 
+	removePrefixSuffix := func(in, prefix string) string {
+		in = strings.TrimPrefix(in, fmt.Sprintf("%s(", prefix))
+		in = strings.TrimSuffix(in, ")")
+
+		return strings.TrimSpace(in)
+	}
+
+	addMapping := func(domain string, rr dns.RR) {
+		if _, ok := result[domain]; !ok {
+			result[domain] = []dns.RR{rr}
+		} else {
+			result[domain] = append(result[domain], rr)
+		}
+	}
+
 	for k, v := range input {
 		for _, part := range strings.Split(v, ",") {
-			if strings.HasPrefix(v, "CNAME:") {
-				domain := strings.TrimSpace(strings.TrimPrefix(v, "CNAME:"))
+			if strings.HasPrefix(part, "CNAME(") {
+				domain := removePrefixSuffix(part, "CNAME")
 				domain = dns.Fqdn(domain)
 				cname := &dns.CNAME{Target: domain}
-
-				if _, ok := result[k]; !ok {
-					result[k] = []dns.RR{cname}
-				} else {
-					result[k] = append(result[k], cname)
-				}
+				addMapping(k, cname)
 			} else {
-				v = strings.TrimPrefix(v, "A:")
-				ip := net.ParseIP(strings.TrimSpace(v))
+				// Fall back to A/AAAA records to maintain backwards compatibility in config.yml
+				// We will still remove the A() or AAAA() if it exists
+				if strings.Contains(part, ".") { // IPV4 address
+					ipStr := removePrefixSuffix(part, "A")
+					ip := net.ParseIP(ipStr)
 
-				if ip == nil {
-					return fmt.Errorf("invalid IP address '%s'", part)
-				}
+					if ip == nil {
+						return fmt.Errorf("inpartalid IP address '%s'", part)
+					}
 
-				a := new(dns.A)
-				a.A = ip
+					a := new(dns.A)
+					a.A = ip
 
-				if _, ok := result[k]; !ok {
-					result[k] = []dns.RR{a}
-				} else {
-					result[k] = append(result[k], a)
+					addMapping(k, a)
+				} else { // IPV6 address
+					ipStr := removePrefixSuffix(part, "AAAA")
+					ip := net.ParseIP(ipStr)
+
+					if ip == nil {
+						return fmt.Errorf("inpartalid IP address '%s'", part)
+					}
+
+					aaaa := new(dns.AAAA)
+					aaaa.AAAA = ip
+
+					addMapping(k, aaaa)
 				}
 			}
 		}
