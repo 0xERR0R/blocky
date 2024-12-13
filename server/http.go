5c40adccd0cde8954c8cdfc0eac4c7b536fa21bd
Theo Technicguy <19630890+TheoTechnicguy@users.noreply.github.com>
2024-12-13 10:10:37 +0100

    feat(server): get read timeout from config

--- server/http.go
+++ server/http.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/0xERR0R/blocky/config"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
 )
@@ -16,16 +17,19 @@ type httpServer struct {
 	name string
 }
 
-func newHTTPServer(name string, handler http.Handler) *httpServer {
+func newHTTPServer(name string, handler http.Handler, cfg *config.Config) *httpServer {
 	const (
 		readHeaderTimeout = 20 * time.Second
-		readTimeout       = 20 * time.Second
 		writeTimeout      = 20 * time.Second
+	)
+
+	var (
+		readTimeout       = cfg.Blocking.Loading.Downloads.ReadTimeout
 	)
 
 	return &httpServer{
 		inner: http.Server{
-			ReadTimeout:       readTimeout,
+			ReadTimeout:       time.Duration(readTimeout),
 			ReadHeaderTimeout: readHeaderTimeout,
 			WriteTimeout:      writeTimeout,
 
