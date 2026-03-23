9ced57280937fa30ac67216392e0053108b68ef6
Dimitri Herzog <dimitri.herzog@gmail.com>
2026-03-23 17:28:18 +0100

    refactor: slim redis package to connection factory, remove cache/pub-sub logic

--- redis/redis_suite_test.go
+++ redis/redis_suite_test.go
@@ -1,25 +1,18 @@
 package redis
 
 import (
-	"context"
 	"testing"
 
 	"github.com/0xERR0R/blocky/log"
-	"github.com/go-redis/redis/v8"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
 func init() {
 	log.Silence()
-	redis.SetLogger(NoLogs{})
 }
 
 func TestRedisClient(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Redis Suite")
 }
-
-type NoLogs struct{}
-
-func (l NoLogs) Printf(context.Context, string, ...any) {}
