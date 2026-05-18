f79efbe3897978bb2737ea204e507d004cf11101
Dimitri Herzog <dimitri.herzog@gmail.com>
2026-05-18 14:39:13 +0200

    feat(config): wire HTTP3 into top-level Config

--- config/http3_test.go
+++ config/http3_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/creasty/defaults"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"gopkg.in/yaml.v2"
 )
 
 var _ = Describe("HTTP3Config", func() {
@@ -38,6 +39,25 @@ var _ = Describe("HTTP3Config", func() {
 
 			Expect(hook.Calls).Should(HaveLen(1))
 			Expect(hook.Messages).Should(ContainElement(ContainSubstring("enabled")))
+		})
+	})
+
+	Describe("Config integration", func() {
+		It("is reachable as Config.HTTP3", func() {
+			c := Config{}
+			Expect(defaults.Set(&c)).Should(Succeed())
+
+			Expect(c.HTTP3.IsEnabled()).Should(BeFalse())
+		})
+
+		It("respects the yaml tag", func() {
+			var c Config
+			Expect(defaults.Set(&c)).Should(Succeed())
+
+			data := []byte("http3:\n  enable: true\n")
+			Expect(yaml.Unmarshal(data, &c)).Should(Succeed())
+
+			Expect(c.HTTP3.IsEnabled()).Should(BeTrue())
 		})
 	})
 })
