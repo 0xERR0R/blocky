0xERR0R/blocky

commit 6259e1326c0c1f827a5bc252da862247d04ac5ee

feat: add Timescale DB with Hypertable and Retention support (#1517)

querylog/database_writer.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"reflect"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -49,16 +50,16 @@ func NewDatabaseWriter(ctx context.Context, dbType, target string, logRetentionD
 ) (*DatabaseWriter, error) {
 	switch dbType {
 	case "mysql":
-		return newDatabaseWriter(ctx, mysql.Open(target), logRetentionDays, dbFlushPeriod)
-	case "postgresql":
-		return newDatabaseWriter(ctx, postgres.Open(target), logRetentionDays, dbFlushPeriod)
+		return newDatabaseWriter(ctx, mysql.Open(target), logRetentionDays, dbFlushPeriod, dbType)
+	case "postgresql", "timescale":
+		return newDatabaseWriter(ctx, postgres.Open(target), logRetentionDays, dbFlushPeriod, dbType)
 	}
 
 	return nil, fmt.Errorf("incorrect database type provided: %s", dbType)
 }
 
 func newDatabaseWriter(ctx context.Context, target gorm.Dialector, logRetentionDays uint64,
-	dbFlushPeriod time.Duration,
+	dbFlushPeriod time.Duration, dbType string,
 ) (*DatabaseWriter, error) {
 	db, err := gorm.Open(target, &gorm.Config{
 		Logger: logger.New(
@@ -75,7 +76,7 @@ func newDatabaseWriter(ctx context.Context, target gorm.Dialector, logRetentionD
 	}
 
 	// Migrate the schema
-	if err := databaseMigration(db); err != nil {
+	if err := databaseMigration(db, dbType, logRetentionDays); err != nil {
 		return nil, fmt.Errorf("can't perform auto migration: %w", err)
 	}
 
@@ -90,7 +91,7 @@ func newDatabaseWriter(ctx context.Context, target gorm.Dialector, logRetentionD
 	return w, nil
 }
 
-func databaseMigration(db *gorm.DB) error {
+func databaseMigration(db *gorm.DB, dbType string, logRetentionDays uint64) error {
 	if err := db.AutoMigrate(&logEntry{}); err != nil {
 		return err
 	}
@@ -98,7 +99,7 @@ func databaseMigration(db *gorm.DB) error {
 	tableName := db.NamingStrategy.TableName(reflect.TypeOf(logEntry{}).Name())
 
 	// create unmapped primary key
-	switch db.Config.Name() {
+	switch dbType {
 	case "mysql":
 		tx := db.Exec("ALTER TABLE `" + tableName + "` ADD `id` INT PRIMARY KEY AUTO_INCREMENT")
 		if tx.Error != nil {
@@ -113,7 +114,30 @@ func databaseMigration(db *gorm.DB) error {
 		}
 
 	case "postgres":
-		return db.Exec("ALTER TABLE " + tableName + " ADD column if not exists id serial primary key").Error
+		return db.Exec("ALTER TABLE " + tableName + " ADD column if not exists id bigserial primary key").Error
+
+	case "timescale":
+		requestTSColName := db.NamingStrategy.ColumnName(reflect.TypeOf(logEntry{}).Name(), "RequestTS")
+
+		// Create a Timescale hypertable
+		tx := db.Exec(`SELECT create_hypertable(
+			'` + tableName + `',
+			by_range('` + requestTSColName + `'),
+			if_not_exists => TRUE
+		)`)
+		if tx.Error != nil {
+			return tx.Error
+		}
+
+		// Create a retention policy for the hypertable
+		tx = db.Exec(`SELECT add_retention_policy(
+			'` + tableName + `',
+			drop_after => INTERVAL '` + strconv.FormatUint(logRetentionDays, 10) + ` days',
+			if_not_exists => TRUE
+		)`)
+		if tx.Error != nil {
+			return tx.Error
+		}
 	}
 
 	return nil

resolver/query_logging_resolver.go
@@ -29,6 +29,34 @@ type QueryLoggingResolver struct {
 	writer  querylog.Writer
 }
 
+func GetQueryLoggingWriter(ctx context.Context, cfg config.QueryLog) (querylog.Writer, error) {
+	var writer querylog.Writer
+
+	var err error
+
+	switch cfg.Type {
+	case config.QueryLogTypeCsv:
+		writer, err = querylog.NewCSVWriter(cfg.Target, false, cfg.LogRetentionDays)
+	case config.QueryLogTypeCsvClient:
+		writer, err = querylog.NewCSVWriter(cfg.Target, true, cfg.LogRetentionDays)
+	case config.QueryLogTypeMysql:
+		writer, err = querylog.NewDatabaseWriter(ctx, "mysql", cfg.Target, cfg.LogRetentionDays,
+			cfg.FlushInterval.ToDuration())
+	case config.QueryLogTypePostgresql:
+		writer, err = querylog.NewDatabaseWriter(ctx, "postgresql", cfg.Target, cfg.LogRetentionDays,
+			cfg.FlushInterval.ToDuration())
+	case config.QueryLogTypeTimescale:
+		writer, err = querylog.NewDatabaseWriter(ctx, "timescale", cfg.Target, cfg.LogRetentionDays,
+			cfg.FlushInterval.ToDuration())
+	case config.QueryLogTypeConsole:
+		writer = querylog.NewLoggerWriter()
+	case config.QueryLogTypeNone:
+		writer = querylog.NewNoneWriter()
+	}
+
+	return writer, err
+}
+
 // NewQueryLoggingResolver returns a new resolver instance
 func NewQueryLoggingResolver(ctx context.Context, cfg config.QueryLog) *QueryLoggingResolver {
 	logger := log.PrefixedLog(queryLoggingResolverType)
@@ -39,22 +67,7 @@ func NewQueryLoggingResolver(ctx context.Context, cfg config.QueryLog) *QueryLog
 		func() error {
 			var err error
 
-			switch cfg.Type {
-			case config.QueryLogTypeCsv:
-				writer, err = querylog.NewCSVWriter(cfg.Target, false, cfg.LogRetentionDays)
-			case config.QueryLogTypeCsvClient:
-				writer, err = querylog.NewCSVWriter(cfg.Target, true, cfg.LogRetentionDays)
-			case config.QueryLogTypeMysql:
-				writer, err = querylog.NewDatabaseWriter(ctx, "mysql", cfg.Target, cfg.LogRetentionDays,
-					cfg.FlushInterval.ToDuration())
-			case config.QueryLogTypePostgresql:
-				writer, err = querylog.NewDatabaseWriter(ctx, "postgresql", cfg.Target, cfg.LogRetentionDays,
-					cfg.FlushInterval.ToDuration())
-			case config.QueryLogTypeConsole:
-				writer = querylog.NewLoggerWriter()
-			case config.QueryLogTypeNone:
-				writer = querylog.NewNoneWriter()
-			}
+			writer, err = GetQueryLoggingWriter(ctx, cfg)
 
 			return err
 		},
@@ -85,7 +98,8 @@ func NewQueryLoggingResolver(ctx context.Context, cfg config.QueryLog) *QueryLog
 
 	go resolver.writeLog(ctx)
 
-	if cfg.LogRetentionDays > 0 {
+	// Timescale uses database features for retention
+	if cfg.LogRetentionDays > 0 && cfg.Type != config.QueryLogTypeTimescale {
 		go resolver.periodicCleanUp(ctx)
 	}
 

e2e/querylog_test.go
@@ -180,6 +180,79 @@ var _ = Describe("Query logs functional tests", func() {
 			})
 		})
 	})
+
+	Describe("Query logging into the timescale database", func() {
+		BeforeEach(func(ctx context.Context) {
+			postgresDB, err = createTimescaleContainer(ctx, e2eNet)
+			Expect(err).Should(Succeed())
+
+			blocky, err = createBlockyContainer(ctx, e2eNet,
+				"log:",
+				"  level: warn",
+				"upstreams:",
+				"  groups:",
+				"    default:",
+				"      - moka1",
+				"queryLog:",
+				"  type: timescale",
+				"  target: postgres://user:user@timescale:5432/user",
+				"  flushInterval: 1s",
+			)
+			Expect(err).Should(Succeed())
+
+			connectionString, err := postgresDB.ConnectionString(ctx, "sslmode=disable")
+			Expect(err).Should(Succeed())
+
+			// database might be slow on first start, retry here if necessary
+			Eventually(gorm.Open, "10s", "1s").
+				WithArguments(postgresDriver.Open(connectionString), &gorm.Config{}).ShouldNot(BeNil())
+
+			db, err = gorm.Open(postgresDriver.Open(connectionString), &gorm.Config{})
+			Expect(err).Should(Succeed())
+
+			Eventually(countEntries).WithArguments(db).Should(BeNumerically("==", 0))
+		})
+		When("Some queries were performed", func() {
+			msg := util.NewMsgWithQuestion("google.de.", dns.Type(dns.TypeA))
+			It("Should store query log in the timescale database", func(ctx context.Context) {
+				By("Performing 2 queries", func() {
+					Expect(doDNSRequest(ctx, blocky, msg)).ShouldNot(BeNil())
+					Expect(doDNSRequest(ctx, blocky, msg)).ShouldNot(BeNil())
+				})
+
+				By("check entries count asynchronously, since blocky flushes log entries in bulk", func() {
+					Eventually(countEntries, "60s", "1s").WithArguments(db).Should(BeNumerically("==", 2))
+				})
+
+				By("check entry content", func() {
+					entries, err := queryEntries(db)
+					Expect(err).Should(Succeed())
+
+					Expect(entries).Should(HaveLen(2))
+
+					Expect(entries[0]).
+						Should(
+							SatisfyAll(
+								HaveField("ResponseType", "RESOLVED"),
+								HaveField("QuestionType", "A"),
+								HaveField("QuestionName", "google.de"),
+								HaveField("Answer", "A (1.2.3.4)"),
+								HaveField("ResponseCode", "NOERROR"),
+							))
+
+					Expect(entries[1]).
+						Should(
+							SatisfyAll(
+								HaveField("ResponseType", "CACHED"),
+								HaveField("QuestionType", "A"),
+								HaveField("QuestionName", "google.de"),
+								HaveField("Answer", "A (1.2.3.4)"),
+								HaveField("ResponseCode", "NOERROR"),
+							))
+				})
+			})
+		})
+	})
 })
 
 type logEntry struct {

querylog/database_writer_test.go
@@ -40,7 +40,7 @@ var _ = Describe("DatabaseWriter", func() {
 
 		When("New log entry was created", func() {
 			BeforeEach(func() {
-				writer, err = newDatabaseWriter(ctx, sqliteDB, 7, time.Millisecond)
+				writer, err = newDatabaseWriter(ctx, sqliteDB, 7, time.Millisecond, "sqlite")
 				Expect(err).Should(Succeed())
 
 				db, err := writer.db.DB()
@@ -91,7 +91,7 @@ var _ = Describe("DatabaseWriter", func() {
 
 		When("> 10000 Entries were created", func() {
 			BeforeEach(func() {
-				writer, err = newDatabaseWriter(ctx, sqliteDB, 7, time.Millisecond)
+				writer, err = newDatabaseWriter(ctx, sqliteDB, 7, time.Millisecond, "sqlite")
 				Expect(err).Should(Succeed())
 			})
 
@@ -122,7 +122,7 @@ var _ = Describe("DatabaseWriter", func() {
 
 		When("There are log entries with timestamp exceeding the retention period", func() {
 			BeforeEach(func() {
-				writer, err = newDatabaseWriter(ctx, sqliteDB, 1, time.Millisecond)
+				writer, err = newDatabaseWriter(ctx, sqliteDB, 1, time.Millisecond, "sqlite")
 				Expect(err).Should(Succeed())
 			})
 
@@ -230,10 +230,10 @@ var _ = Describe("DatabaseWriter", func() {
 				})
 
 				By("create postgres specific manually defined primary key", func() {
-					mock.ExpectExec(`ALTER TABLE log_entries ADD column if not exists id serial primary key`).WillReturnResult(sqlmock.NewResult(0, 0))
+					mock.ExpectExec(`ALTER TABLE log_entries ADD column if not exists id bigserial primary key`).WillReturnResult(sqlmock.NewResult(0, 0))
 				})
 
-				_, err = newDatabaseWriter(ctx, dlc, 1, time.Millisecond)
+				_, err = newDatabaseWriter(ctx, dlc, 1, time.Millisecond, "postgres")
 				Expect(err).Should(Succeed())
 			})
 		})
@@ -265,7 +265,7 @@ var _ = Describe("DatabaseWriter", func() {
 						mock.ExpectExec("ALTER TABLE `log_entries` ADD `id` INT PRIMARY KEY AUTO_INCREMENT").WillReturnResult(sqlmock.NewResult(0, 0))
 					})
 
-					_, err = newDatabaseWriter(ctx, dlc, 1, time.Millisecond)
+					_, err = newDatabaseWriter(ctx, dlc, 1, time.Millisecond, "mysql")
 					Expect(err).Should(Succeed())
 				})
 			})
@@ -281,7 +281,7 @@ var _ = Describe("DatabaseWriter", func() {
 						mock.ExpectExec("ALTER TABLE `log_entries` ADD `id` INT PRIMARY KEY AUTO_INCREMENT").WillReturnError(fmt.Errorf("error 1060: duplicate column name"))
 					})
 
-					_, err = newDatabaseWriter(ctx, dlc, 1, time.Millisecond)
+					_, err = newDatabaseWriter(ctx, dlc, 1, time.Millisecond, "mysql")
 					Expect(err).Should(Succeed())
 				})
 
@@ -294,7 +294,7 @@ var _ = Describe("DatabaseWriter", func() {
 						mock.ExpectExec("ALTER TABLE `log_entries` ADD `id` INT PRIMARY KEY AUTO_INCREMENT").WillReturnError(fmt.Errorf("error XXX: some index error"))
 					})
 
-					_, err = newDatabaseWriter(ctx, dlc, 1, time.Millisecond)
+					_, err = newDatabaseWriter(ctx, dlc, 1, time.Millisecond, "mysql")
 					Expect(err).Should(HaveOccurred())
 					Expect(err.Error()).Should(ContainSubstring("can't perform auto migration: error XXX: some index error"))
 				})
@@ -306,7 +306,7 @@ var _ = Describe("DatabaseWriter", func() {
 						mock.ExpectExec("CREATE TABLE `log_entries`").WillReturnError(fmt.Errorf("error XXX: some db error"))
 					})
 
-					_, err = newDatabaseWriter(ctx, dlc, 1, time.Millisecond)
+					_, err = newDatabaseWriter(ctx, dlc, 1, time.Millisecond, "mysql")
 					Expect(err).Should(HaveOccurred())
 					Expect(err.Error()).Should(ContainSubstring("can't perform auto migration: error XXX: some db error"))
 				})

config/config.go
@@ -108,6 +108,7 @@ func (v *TLSVersion) validate(logger *logrus.Entry) {
 // postgresql // PostgreSQL database
 // csv // CSV file per day
 // csv-client // CSV file per day and client
+// timescale // Timescale database
 // )
 type QueryLogType int16
 
@@ -466,7 +467,6 @@ func loadConfig(logger *logrus.Entry, path string, mandatory bool) (rCfg *Config
 		prettyPath = filepath.Join(path, "*")
 
 		data, err = readFromDir(path, data)
-
 		if err != nil {
 			return nil, fmt.Errorf("can't read config files: %w", err)
 		}

commit 1b28ce9d0d7aef030c7085cca551444670f8e5b5

fix(querylog): harden sqlite query log and use typed QueryLogType
Addresses review feedback on the sqlite query log:

- buildSQLiteDSN: percent-encode %, ? and # in the target path. The
  "file:" URI mode runs the path through SQLite's URI parser, so a path
  containing "?" or "#" was truncated (opening a different file, "?" also
  silently disabling WAL) and "%xx" was decoded into a different path.
- sqlite: pin the connection pool to a single connection so blocky's own
  periodic flush and retention cleanup can't collide into SQLITE_BUSY.
- Pass config.QueryLogType through NewDatabaseWriter/databaseMigration
  instead of magic strings, removing the dialector type assertion.
- Restore the postgres "id" primary key migration. It regressed in #1517
  (v0.25): the migration switch moved from db.Config.Name() ("postgres")
  to the dbType string ("postgresql") without updating the case, so the
  ALTER silently stopped running for postgres.
- Add an explicit sqlite migration case documenting that it relies on
  SQLite's implicit rowid as the primary key.
- tests: deterministic queryEntries ordering; cancel the context before
  closing the DB in the WAL test so the flush goroutine can't write to a
  closed connection.
- docs: sqlite target is a plain path (not "file:"), WAL needs a local
  filesystem, and retention deletes rows but VACUUM is needed to reclaim
  disk space.

config/query_log.go
@@ -22,7 +22,8 @@ type QueryLog struct {
 	CreationCooldown Duration `default:"2s" yaml:"creationCooldown"`
 	// Which fields to include in log entries; defaults to all available fields.
 	Fields []QueryLogField `yaml:"fields"`
-	// Interval at which buffered log entries are flushed to the external database.
+	// Interval at which buffered log entries are flushed in bulk to the database
+	// (used by the mysql, postgresql, timescale and sqlite targets).
 	FlushInterval Duration `default:"30s" yaml:"flushInterval"`
 	// Rules to suppress certain queries from being logged.
 	Ignore QueryLogIgnore `yaml:"ignore"`

querylog/database_writer.go
@@ -14,6 +14,7 @@ import (
 
 	"gorm.io/gorm/logger"
 
+	"github.com/0xERR0R/blocky/config"
 	"github.com/0xERR0R/blocky/log"
 	"github.com/hashicorp/go-multierror"
 
@@ -58,20 +59,27 @@ const sqliteDirPermission os.FileMode = 0o750
 
 // buildSQLiteDSN turns a filesystem path into a modernc/glebarez SQLite DSN with
 // WAL journaling enabled. The "file:" prefix selects SQLite's URI filename mode,
-// the canonical form for passing connection options as query parameters.
+// the canonical form for passing connection options as query parameters. Because
+// that mode runs the path through SQLite's URI parser, the path is percent-encoded
+// first: otherwise a path containing "?" or "#" would be truncated (silently
+// opening a different file, possibly without WAL) and "%xx" would be decoded into
+// a different path. "%" is encoded first as it is the escape character itself;
+// strings.Replacer never re-scans its own output, so the inserted "%25" is safe.
 func buildSQLiteDSN(path string) string {
-	return fmt.Sprintf("file:%s?_pragma=journal_mode(WAL)&_pragma=busy_timeout(%d)", path, sqliteBusyTimeoutMs)
+	encodedPath := strings.NewReplacer("%", "%25", "?", "%3F", "#", "%23").Replace(path)
+
+	return fmt.Sprintf("file:%s?_pragma=journal_mode(WAL)&_pragma=busy_timeout(%d)", encodedPath, sqliteBusyTimeoutMs)
 }
 
-func NewDatabaseWriter(ctx context.Context, dbType, target string, logRetentionDays uint64,
+func NewDatabaseWriter(ctx context.Context, dbType config.QueryLogType, target string, logRetentionDays uint64,
 	dbFlushPeriod time.Duration,
 ) (*DatabaseWriter, error) {
-	switch dbType {
-	case "mysql":
+	switch dbType { //nolint:exhaustive // non-database query-log types are handled in GetQueryLoggingWriter
+	case config.QueryLogTypeMysql:
 		return newDatabaseWriter(ctx, mysql.Open(target), logRetentionDays, dbFlushPeriod, dbType)
-	case "postgresql", "timescale":
+	case config.QueryLogTypePostgresql, config.QueryLogTypeTimescale:
 		return newDatabaseWriter(ctx, postgres.Open(target), logRetentionDays, dbFlushPeriod, dbType)
-	case "sqlite":
+	case config.QueryLogTypeSqlite:
 		if target == "" {
 			return nil, errors.New("sqlite query log requires a target file path")
 		}
@@ -87,7 +95,7 @@ func NewDatabaseWriter(ctx context.Context, dbType, target string, logRetentionD
 }
 
 func newDatabaseWriter(ctx context.Context, target gorm.Dialector, logRetentionDays uint64,
-	dbFlushPeriod time.Duration, dbType string,
+	dbFlushPeriod time.Duration, dbType config.QueryLogType,
 ) (*DatabaseWriter, error) {
 	db, err := gorm.Open(target, &gorm.Config{
 		Logger: logger.New(
@@ -101,6 +109,20 @@ func newDatabaseWriter(ctx context.Context, target gorm.Dialector, logRetentionD
 	})
 	if err != nil {
 		return nil, fmt.Errorf("can't create database connection: %w", err)
+	}
+
+	// SQLite is a single local file: a write holds an exclusive lock on the whole
+	// database, so letting the pool open several connections only turns blocky's own
+	// concurrent access (the periodic flush vs. the retention cleanup) into
+	// SQLITE_BUSY errors. Serialize through one connection instead; external readers
+	// use their own connections and are unaffected.
+	if dbType == config.QueryLogTypeSqlite {
+		sqlDB, err := db.DB()
+		if err != nil {
+			return nil, fmt.Errorf("can't access sqlite connection pool: %w", err)
+		}
+
+		sqlDB.SetMaxOpenConns(1)
 	}
 
 	// Migrate the schema
@@ -119,7 +141,7 @@ func newDatabaseWriter(ctx context.Context, target gorm.Dialector, logRetentionD
 	return w, nil
 }
 
-func databaseMigration(db *gorm.DB, dbType string, logRetentionDays uint64) error {
+func databaseMigration(db *gorm.DB, dbType config.QueryLogType, logRetentionDays uint64) error {
 	if err := db.AutoMigrate(&logEntry{}); err != nil {
 		return fmt.Errorf("failed to auto-migrate database schema for querylog: %w", err)
 	}
@@ -127,8 +149,13 @@ func databaseMigration(db *gorm.DB, dbType string, logRetentionDays uint64) erro
 	tableName := db.NamingStrategy.TableName(reflect.TypeFor[logEntry]().Name())
 
 	// create unmapped primary key
-	switch dbType {
-	case "mysql":
+	switch dbType { //nolint:exhaustive // only database-backed targets reach migration
+	case config.QueryLogTypeSqlite:
+		// SQLite gives every table an implicit auto-incrementing rowid that already
+		// acts as the primary key, so unlike the other targets no extra id column is
+		// added here (and SQLite cannot ALTER TABLE ... ADD a PRIMARY KEY column).
+
+	case config.QueryLogTypeMysql:
 		tx := db.Exec("ALTER TABLE `" + tableName + "` ADD `id` INT PRIMARY KEY AUTO_INCREMENT")
 		if tx.Error != nil {
 			// mysql doesn't support "add column if not exist"
@@ -141,10 +168,10 @@ func databaseMigration(db *gorm.DB, dbType string, logRetentionDays uint64) erro
 			return tx.Error
 		}
 
-	case "postgres":
+	case config.QueryLogTypePostgresql:
 		return db.Exec("ALTER TABLE " + tableName + " ADD column if not exists id bigserial primary key").Error
 
-	case "timescale":
+	case config.QueryLogTypeTimescale:
 		requestTSColName := db.NamingStrategy.ColumnName(reflect.TypeFor[logEntry]().Name(), "RequestTS")
 
 		// Create a Timescale hypertable

resolver/query_logging_resolver.go
@@ -45,17 +45,10 @@ func GetQueryLoggingWriter(ctx context.Context, cfg config.QueryLog) (querylog.W
 		writer, err = querylog.NewCSVWriter(cfg.Target.Reveal(), false, cfg.LogRetentionDays)
 	case config.QueryLogTypeCsvClient:
 		writer, err = querylog.NewCSVWriter(cfg.Target.Reveal(), true, cfg.LogRetentionDays)
-	case config.QueryLogTypeMysql:
-		writer, err = querylog.NewDatabaseWriter(ctx, "mysql", cfg.Target.Reveal(), cfg.LogRetentionDays,
-			cfg.FlushInterval.ToDuration())
-	case config.QueryLogTypePostgresql:
-		writer, err = querylog.NewDatabaseWriter(ctx, "postgresql", cfg.Target.Reveal(), cfg.LogRetentionDays,
-			cfg.FlushInterval.ToDuration())
-	case config.QueryLogTypeTimescale:
-		writer, err = querylog.NewDatabaseWriter(ctx, "timescale", cfg.Target.Reveal(), cfg.LogRetentionDays,
-			cfg.FlushInterval.ToDuration())
-	case config.QueryLogTypeSqlite:
-		writer, err = querylog.NewDatabaseWriter(ctx, "sqlite", cfg.Target.Reveal(), cfg.LogRetentionDays,
+	case config.QueryLogTypeMysql, config.QueryLogTypePostgresql, config.QueryLogTypeTimescale, config.QueryLogTypeSqlite:
+		// The database-backed targets share one writer; it selects the driver and
+		// migration from the typed QueryLogType, so no per-type string is threaded through.
+		writer, err = querylog.NewDatabaseWriter(ctx, cfg.Type, cfg.Target.Reveal(), cfg.LogRetentionDays,
 			cfg.FlushInterval.ToDuration())
 	case config.QueryLogTypeConsole:
 		writer = querylog.NewLoggerWriter()

e2e/querylog_test.go
@@ -345,7 +345,10 @@ type logEntry struct {
 func queryEntries(db *gorm.DB) ([]logEntry, error) {
 	var entries []logEntry
 
-	return entries, db.Find(&entries).Order("request_ts DESC").Error
+	// Returned in insertion order: the tests issue their queries sequentially and
+	// assert on that order. We intentionally don't add an ORDER BY request_ts, which
+	// can tie at the column's time resolution and make the order non-deterministic.
+	return entries, db.Find(&entries).Error
 }
 
 func countEntries(db *gorm.DB) (int64, error) {

querylog/database_writer_test.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/0xERR0R/blocky/config"
 	"github.com/DATA-DOG/go-sqlmock"
 
 	"github.com/glebarez/sqlite"
@@ -41,7 +42,7 @@ var _ = Describe("DatabaseWriter", func() {
 
 		When("New log entry was created", func() {
 			BeforeEach(func() {
-				writer, err = newDatabaseWriter(ctx, sqliteDB, 7, time.Millisecond, "sqlite")
+				writer, err = newDatabaseWriter(ctx, sqliteDB, 7, time.Millisecond, config.QueryLogTypeSqlite)
 				Expect(err).Should(Succeed())
 
 				db, err := writer.db.DB()
@@ -92,7 +93,7 @@ var _ = Describe("DatabaseWriter", func() {
 
 		When("> 10000 Entries were created", func() {
 			BeforeEach(func() {
-				writer, err = newDatabaseWriter(ctx, sqliteDB, 7, time.Millisecond, "sqlite")
+				writer, err = newDatabaseWriter(ctx, sqliteDB, 7, time.Millisecond, config.QueryLogTypeSqlite)
 				Expect(err).Should(Succeed())
 			})
 
@@ -123,7 +124,7 @@ var _ = Describe("DatabaseWriter", func() {
 
 		When("There are log entries with timestamp exceeding the retention period", func() {
 			BeforeEach(func() {
-				writer, err = newDatabaseWriter(ctx, sqliteDB, 1, time.Millisecond, "sqlite")
+				writer, err = newDatabaseWriter(ctx, sqliteDB, 1, time.Millisecond, config.QueryLogTypeSqlite)
 				Expect(err).Should(Succeed())
 			})
 
@@ -172,13 +173,22 @@ var _ = Describe("DatabaseWriter", func() {
 		It("creates the file (and parent dir), enables WAL, and persists entries", func() {
 			dbPath := filepath.Join(GinkgoT().TempDir(), "sub", "querylog.db") // 'sub' exercises mkdir
 
-			w, err := NewDatabaseWriter(ctx, "sqlite", dbPath, 7, time.Millisecond)
+			w, err := NewDatabaseWriter(ctx, config.QueryLogTypeSqlite, dbPath, 7, time.Millisecond)
 			Expect(err).Should(Succeed())
 
 			sqlDB, err := w.db.DB()
 			Expect(err).Should(Succeed())
-			sqlDB.SetMaxOpenConns(1)
-			DeferCleanup(sqlDB.Close)
+
+			// Cancel the context (stopping the periodic-flush goroutine) before closing
+			// the DB, so the goroutine can't issue a write against a closed connection
+			// during teardown. DeferCleanup runs LIFO, so registering this after the
+			// BeforeEach's cancelFn makes it run first. (NewDatabaseWriter already pins
+			// the sqlite pool to a single connection.)
+			DeferCleanup(func() error {
+				cancelFn()
+
+				return sqlDB.Close()
+			})
 
 			w.Write(&LogEntry{Start: time.Now(), DurationMs: 20})
 
@@ -193,8 +203,30 @@ var _ = Describe("DatabaseWriter", func() {
 			Expect(dbPath + "-wal").Should(BeAnExistingFile())
 		})
 
+		It("opens the literal path even when it contains URI-special characters", func() {
+			// '?' and '#' used to truncate the path (opening a different file, '?'
+			// also silently disabling WAL) and '%xx' used to be decoded into another
+			// path. The DSN now percent-encodes these, so the intended file is opened.
+			dbPath := filepath.Join(GinkgoT().TempDir(), "q?l#x%y.db")
+
+			w, err := NewDatabaseWriter(ctx, config.QueryLogTypeSqlite, dbPath, 7, time.Minute)
+			Expect(err).Should(Succeed())
+
+			sqlDB, err := w.db.DB()
+			Expect(err).Should(Succeed())
+			DeferCleanup(func() error {
+				cancelFn()
+
+				return sqlDB.Close()
+			})
+
+			Expect(dbPath).Should(BeAnExistingFile())
+			// -wal next to the intended file confirms WAL was applied to it
+			Expect(dbPath + "-wal").Should(BeAnExistingFile())
+		})
+
 		It("returns an error when the target path is empty", func() {
-			_, err := NewDatabaseWriter(ctx, "sqlite", "", 7, time.Millisecond)
+			_, err := NewDatabaseWriter(ctx, config.QueryLogTypeSqlite, "", 7, time.Millisecond)
 			Expect(err).Should(HaveOccurred())
 			Expect(err.Error()).Should(ContainSubstring("sqlite query log requires a target"))
 		})
@@ -203,7 +235,7 @@ var _ = Describe("DatabaseWriter", func() {
 	Describe("Database query log fails", func() {
 		When("mysql connection parameters wrong", func() {
 			It("should be log with fatal", func() {
-				_, err := NewDatabaseWriter(ctx, "mysql", "wrong param", 7, 1)
+				_, err := NewDatabaseWriter(ctx, config.QueryLogTypeMysql, "wrong param", 7, 1)
 				Expect(err).Should(HaveOccurred())
 				Expect(err.Error()).Should(HavePrefix("can't create database connection"))
 			})
@@ -211,7 +243,7 @@ var _ = Describe("DatabaseWriter", func() {
 
 		When("postgresql connection parameters wrong", func() {
 			It("should be log with fatal", func() {
-				_, err := NewDatabaseWriter(ctx, "postgresql", "wrong param", 7, 1)
+				_, err := NewDatabaseWriter(ctx, config.QueryLogTypePostgresql, "wrong param", 7, 1)
 				Expect(err).Should(HaveOccurred())
 				Expect(err.Error()).Should(HavePrefix("can't create database connection"))
 			})
@@ -219,7 +251,7 @@ var _ = Describe("DatabaseWriter", func() {
 
 		When("invalid database type is specified", func() {
 			It("should be log with fatal", func() {
-				_, err := NewDatabaseWriter(ctx, "invalidsql", "", 7, 1)
+				_, err := NewDatabaseWriter(ctx, config.QueryLogType(-1), "", 7, 1)
 				Expect(err).Should(HaveOccurred())
 				Expect(err.Error()).Should(HavePrefix("incorrect database type provided"))
 			})
@@ -272,7 +304,7 @@ var _ = Describe("DatabaseWriter", func() {
 					mock.ExpectExec(`ALTER TABLE log_entries ADD column if not exists id bigserial primary key`).WillReturnResult(sqlmock.NewResult(0, 0))
 				})
 
-				_, err = newDatabaseWriter(ctx, dlc, 1, time.Millisecond, "postgres")
+				_, err = newDatabaseWriter(ctx, dlc, 1, time.Millisecond, config.QueryLogTypePostgresql)
 				Expect(err).Should(Succeed())
 			})
 		})
@@ -303,7 +335,7 @@ var _ = Describe("DatabaseWriter", func() {
 						mock.ExpectExec("ALTER TABLE `log_entries` ADD `id` INT PRIMARY KEY AUTO_INCREMENT").WillReturnResult(sqlmock.NewResult(0, 0))
 					})
 
-					_, err = newDatabaseWriter(ctx, dlc, 1, time.Millisecond, "mysql")
+					_, err = newDatabaseWriter(ctx, dlc, 1, time.Millisecond, config.QueryLogTypeMysql)
 					Expect(err).Should(Succeed())
 				})
 			})
@@ -318,7 +350,7 @@ var _ = Describe("DatabaseWriter", func() {
 						mock.ExpectExec("ALTER TABLE `log_entries` ADD `id` INT PRIMARY KEY AUTO_INCREMENT").WillReturnError(errors.New("error 1060: duplicate column name"))
 					})
 
-					_, err = newDatabaseWriter(ctx, dlc, 1, time.Millisecond, "mysql")
+					_, err = newDatabaseWriter(ctx, dlc, 1, time.Millisecond, config.QueryLogTypeMysql)
 					Expect(err).Should(Succeed())
 				})
 
@@ -331,7 +363,7 @@ var _ = Describe("DatabaseWriter", func() {
 						mock.ExpectExec("ALTER TABLE `log_entries` ADD `id` INT PRIMARY KEY AUTO_INCREMENT").WillReturnError(errors.New("error XXX: some index error"))
 					})
 
-					_, err = newDatabaseWriter(ctx, dlc, 1, time.Millisecond, "mysql")
+					_, err = newDatabaseWriter(ctx, dlc, 1, time.Millisecond, config.QueryLogTypeMysql)
 					Expect(err).Should(HaveOccurred())
 					Expect(err.Error()).Should(ContainSubstring("error XXX: some index error"))
 				})
@@ -343,7 +375,7 @@ var _ = Describe("DatabaseWriter", func() {
 						mock.ExpectExec("CREATE TABLE `log_entries`").WillReturnError(errors.New("error XXX: some db error"))
 					})
 
-					_, err = newDatabaseWriter(ctx, dlc, 1, time.Millisecond, "mysql")
+					_, err = newDatabaseWriter(ctx, dlc, 1, time.Millisecond, config.QueryLogTypeMysql)
 					Expect(err).Should(HaveOccurred())
 					Expect(err.Error()).Should(ContainSubstring("error XXX: some db error"))
 				})